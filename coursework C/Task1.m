b074c7b5ee1701252898fe1534846e9b323862cd
Thomas Bos <thymer.bos217@gmail.com>
2021-06-04 20:28:38 +0200

    Fixed reconstruction error and vanilla pca

--- coursework C/Task1.m
+++ coursework C/Task1.m
@@ -1,25 +1,71 @@
 function Task1()
     DrivFace = load("data/DrivFace/DrivFace.mat"); % http://archive.ics.uci.edu/ml/datasets/DrivFace
     DFdata = DrivFace.drivFaceD.data;
-    [M_res, e] = pca(DFdata, 10, true);
-    e
+    
+    ds = [1,2,4,8,16,32,64,128,256,512];
+    es_pca = zeros(10,1);
+    es_pca_gram = zeros(10,1);
+    es_pca_matlab = zeros(10,1);
+    
+    for i = 1:10
+        ds(i)
+        [~, e_pca] = pca_vanilla(DFdata, ds(i), true);
+        [~, e_pca_gram] = pca_gram(DFdata, ds(i), true);
+        
+        pca_matlab = pca(DFdata);
+        e_pca_matlab = reconstruction_error(DFdata, pca_matlab(:,1:ds(i)));
+        
+        es_pca(i) = e_pca;
+        es_pca_gram(i) = e_pca_gram;
+        es_pca_matlab(i) = e_pca_matlab;
+    end
+    
+    es_pca
+    es_pca_gram
+    es_pca_matlab
 end
 
-function [M_res, e] = pca(m, d, compute_error)
-    mc = detrend(m, 'constant');
-    mean(mc, 1); % Very close to [0]^N but not quite. 
+function [basis_vecs, e] = pca_vanilla(m, d, compute_error)
+    m_mean = mean(m, 1);
+    mc = m;
+    for i=1:size(m,1)
+        mc(i,:) = mc(i,:) - m_mean;
+    end
     C = cov(mc);
     [V, D] = eig(C);
-    [B, I] = maxk(maxk(D, 1), d); % Bit useless as the last `d` eigen values are the biggest
-    feat_vec = V(:,end-d+1:end);
-    M_res = (feat_vec.' * m.').';
+    [~, ind] = sort(diag(D), 'descend'); % Fetch the top d eigenvalues and indices
+    basis_vecs = V(:,ind(1:d)); % Select the top d eigenvectors
     if compute_error
-        e = reconstruction_error(m, M_res, feat_vec);
+        e = reconstruction_error(m, basis_vecs);
     end
 end
 
-function e = reconstruction_error(m, M_res, feat_vec)
-    M_hat = M_res * feat_vec.';
-    error_mat = norm(m - M_hat, 'fro');
+function [basis_vecs, e] = pca_gram(m, d, compute_error)
+    m_mean = mean(m, 1);                            % Trivial way to compute mean
+    mc = m;
+    for i=1:size(m,1)
+        mc(i,:) = mc(i,:) - m_mean;
+    end
+    [V, D] = eig(1/(size(mc, 1))*(mc*mc.'));        % Calculate eigenvalues of (1/m)YY^T (we have row data)
+    [D_sorted, ind] = sort(diag(D), 'descend');     % Fetch the top d eigenvalues and indices
+    eigen_vecs = V(:,ind(1:d));                     % Select the d best eigenvectors
+    basis_vecs = zeros(size(mc, 2), d);             % Pre-allocate
+    for i = 1:d
+        eigenvalue = D_sorted(i);
+        basis_vecs(:,i) = (1/(eigenvalue^0.5)) * mc.' * eigen_vecs(:,i); % Calculate basis vector i
+    end
+    if compute_error
+        e = reconstruction_error(m, basis_vecs);
+    end
+end
+
+function e = reconstruction_error(m, basis_vecs)
+    m_mean = mean(m, 1);
+    mc = m;
+    for i=1:size(m,1)
+        mc(i,:) = mc(i,:) - m_mean;
+    end
+    MC_res = mc * (basis_vecs * basis_vecs.');
+    error_mat = norm(mc - MC_res, 'fro');
     e = sum(error_mat);
 end
