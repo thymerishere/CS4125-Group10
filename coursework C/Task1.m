9025e42662ea8a35baadad3987ae89ccb78453e7
Thomas Bos <thymer.bos217@gmail.com>
2021-06-07 10:38:47 +0200

    Added matlab pca for comparison

--- coursework C/Task1.m
+++ coursework C/Task1.m
@@ -2,35 +2,50 @@ function Task1()
     DrivFace = load("data/DrivFace/DrivFace.mat"); % http://archive.ics.uci.edu/ml/datasets/DrivFace
     DFdata = DrivFace.drivFaceD.data;
     
+    TCGA = csvread("data/TCGA-PANCAN-HiSeq-801x20531/data.csv", 1, 1); % http://archive.ics.uci.edu/ml/machine-learning-databases/00401/
+    TCGA = TCGA(:,1:5000); % Take a subset due to processor melting
+    
     ds = [1,2,4,8,16,32,64,128,256,512];
-    es_pca = zeros(10,1);
-    es_pca_gram = zeros(10,1);
-    es_pca_matlab = zeros(10,1);
+    es_pca_df = zeros(10,1);
+    es_pca_gram_df = zeros(10,1);
+    es_pca_matlab_df = zeros(10,1);
+    
+    es_pca_tc = zeros(10,1);
+    es_pca_gram_tc = zeros(10,1);
+    es_pca_matlab_tc = zeros(10,1);
+    
+    
+    dfc = center(DFdata);
+    tcc = center(TCGA);
     
     for i = 1:10
         ds(i)
-        [~, e_pca] = pca_vanilla(DFdata, ds(i), true);
-        [~, e_pca_gram] = pca_gram(DFdata, ds(i), true);
+        [~, e_pca_df] = pca_vanilla(DFdata, dfc, ds(i), true);
+        [~, e_pca_gram_df] = pca_gram(DFdata, dfc, ds(i), true);
         
-        pca_matlab = pca(DFdata);
-        e_pca_matlab = reconstruction_error(DFdata, pca_matlab(:,1:ds(i)));
+        pca_matlab_df = pca(DFdata);
+        e_pca_matlab_df = reconstruction_error(DFdata, pca_matlab_df(:,1:ds(i)));
         
-        es_pca(i) = e_pca;
-        es_pca_gram(i) = e_pca_gram;
-        es_pca_matlab(i) = e_pca_matlab;
+        es_pca_df(i) = e_pca_df;
+        es_pca_gram_df(i) = e_pca_gram_df;
+        es_pca_matlab_df(i) = e_pca_matlab_df;
+        
+        [~, e_pca_tc] = pca_vanilla(TCGA, tcc, ds(i), true);
+        [~, e_pca_gram_tc] = pca_gram(TCGA, tcc, ds(i), true);
+        
+        pca_matlab_tc = pca(TCGA);
+        e_pca_matlab_tc = reconstruction_error(TCGA, pca_matlab_tc(:,1:ds(i)));
+        
+        es_pca_tc(i) = e_pca_tc;
+        es_pca_gram_tc(i) = e_pca_gram_tc;
+        es_pca_matlab_tc(i) = e_pca_matlab_tc;
     end
     
-    es_pca
-    es_pca_gram
-    es_pca_matlab
+    es_pca_df, es_pca_gram_df, es_pca_matlab_df
+    es_pca_tc, es_pca_gram_tc, es_pca_matlab_tc
 end
 
-function [basis_vecs, e] = pca_vanilla(m, d, compute_error)
-    m_mean = mean(m, 1);
-    mc = m;
-    for i=1:size(m,1)
-        mc(i,:) = mc(i,:) - m_mean;
-    end
+function [basis_vecs, e] = pca_vanilla(m, mc, d, compute_error)
     C = cov(mc);
     [V, D] = eig(C);
     [~, ind] = sort(diag(D), 'descend'); % Fetch the top d eigenvalues and indices
@@ -40,12 +55,7 @@ function [basis_vecs, e] = pca_vanilla(m, d, compute_error)
     end
 end
 
-function [basis_vecs, e] = pca_gram(m, d, compute_error)
-    m_mean = mean(m, 1);                            % Trivial way to compute mean
-    mc = m;
-    for i=1:size(m,1)
-        mc(i,:) = mc(i,:) - m_mean;
-    end
+function [basis_vecs, e] = pca_gram(m, mc, d, compute_error)
     [V, D] = eig(1/(size(mc, 1))*(mc*mc.'));        % Calculate eigenvalues of (1/m)YY^T (we have row data)
     [D_sorted, ind] = sort(diag(D), 'descend');     % Fetch the top d eigenvalues and indices
     eigen_vecs = V(:,ind(1:d));                     % Select the d best eigenvectors
@@ -60,12 +70,16 @@ function [basis_vecs, e] = pca_gram(m, d, compute_error)
 end
 
 function e = reconstruction_error(m, basis_vecs)
-    m_mean = mean(m, 1);
+    mc = center(m);
+    MC_res = mc * (basis_vecs * basis_vecs.');
+    error_mat = norm(mc - MC_res, 'fro');
+    e = sum(error_mat);
+end
+
+function mc = center(m)
+    m_mean = mean(m, 1);                            
     mc = m;
     for i=1:size(m,1)
         mc(i,:) = mc(i,:) - m_mean;
     end
-    MC_res = mc * (basis_vecs * basis_vecs.');
-    error_mat = norm(mc - MC_res, 'fro');
-    e = sum(error_mat);
 end
